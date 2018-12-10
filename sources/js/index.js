2afcce00ec8ecac1996ccb5c5492de7473a4aed6
Grzegorz Tomicki <gtomicki@gmail.com>
2018-12-10 20:03:04 +0100

    add snow ;)

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,6 +1,7 @@
 import './modules/foreachPolyfill';
 import './modules/scrolltop';
 import './modules/nav';
+import './modules/snow';
 
 import '../../sources/scss/modules/_gallery.scss';
 import '../../sources/scss/modules/_grid.scss';
