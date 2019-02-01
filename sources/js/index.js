494653173525435823abbbd92ebee12331a2ba84
Grzegorz Tomicki <gtomicki@gmail.com>
2019-02-01 15:09:12 +0100

    remove snow

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,7 +1,7 @@
 import './modules/foreachPolyfill';
 import './modules/scrolltop';
 import './modules/nav';
-import './modules/snow';
+// import './modules/snow';
 
 import '../../sources/scss/modules/_gallery.scss';
 import '../../sources/scss/modules/_grid.scss';
