4aaeb70c885bc1565b164032592b8006a570c27d
Grzegorz Tomicki <gtomicki@gmail.com>
2019-07-27 18:57:23 +0200

    removing unnecessary files

--- sources/js/modules/foreachPolyfill.js
+++
@@ -1,5 +0,0 @@
-// https://stackoverflow.com/questions/44652092/ie-doesnt-support-foreach-even-with-polyfill
-(function() {
-  if (typeof NodeList.prototype.forEach === 'function') return false;
-  NodeList.prototype.forEach = Array.prototype.forEach;
-})();
