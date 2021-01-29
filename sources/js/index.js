39dc12fee8b774f301ec8c272e96af6c3a39f5af
Grzegorz Tomicki <gtomik@gmail.com>
2021-01-29 17:08:56 +0100

    refactor: update

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,4 +1,5 @@
 import 'styles/modules/_gallery.scss';
 import 'styles/modules/_grid.scss';
 import 'styles/style.scss';
-import './modules/Observer';
+
+import './share';
--- sources/js/share.js
+++ sources/js/share.js
@@ -1,9 +1,8 @@
 import mail from './modules/Mail';
 import backToTop from './modules/BackToTop';
 import smoothScroll from './modules/SmoothScroll';
+import './modules/Observer';
 
-document.addEventListener('DOMContentLoaded', () => {
-  mail();
-  backToTop();
-  smoothScroll();
-});
+mail();
+backToTop();
+smoothScroll();
