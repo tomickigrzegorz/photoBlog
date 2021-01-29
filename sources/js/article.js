d54b5eb9f74541fa3028eac2a8050971d29267fd
Grzegorz Tomicki <gtomik@gmail.com>
2021-01-29 17:08:56 +0100

    refactor: update

--- sources/js/article.js
+++ sources/js/article.js
@@ -1,15 +1,13 @@
 import 'styles/modules/_article.scss';
 import 'styles/style.scss';
-import './modules/Observer';
+
 import DisqusLoader from './modules/Disqusloader';
 import ShareButton from './modules/ShareButton';
-// import Zooom from './modules/zooom.umd.min';
-// import ReadingTime from './modules/readingTime.umd.min';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sharebutton = new ShareButton();
-  sharebutton.initial();
+import './share';
 
-  const disqusloader = new DisqusLoader();
-  disqusloader.initial();
-});
+const sharebutton = new ShareButton();
+sharebutton.initial();
+
+const disqusloader = new DisqusLoader();
+disqusloader.initial();
