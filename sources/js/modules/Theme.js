a9f6c44539a9e13ce5188821d4734d060f6f2c04
Grzegorz Tomicki <gtomik@gmail.com>
2021-02-17 22:01:19 +0100

    feat: ARIA button

--- sources/js/modules/Theme.js
+++ sources/js/modules/Theme.js
@@ -3,6 +3,10 @@ const toggleSwitch = document.querySelector('.theme');
 toggleSwitch.addEventListener('click', () => {
   const currentTheme = localStorage.getItem('theme');
   const typeTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  toggleSwitch.setAttribute(
+    'aria-label',
+    `zmień motyw na ${currentTheme === 'dark' ? 'dark' : 'light'}`
+  );
   document.body.setAttribute('data-theme', typeTheme);
   localStorage.setItem('theme', typeTheme);
 });
