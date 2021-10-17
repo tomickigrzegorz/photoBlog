019760998694e9b82375e90ae7a5c29485a8dabc
Grzegorz Tomicki <gtomik@gmail.com>
2021-10-17 10:27:04 +0200

    removed observer

--- sources/js/share.js
+++ sources/js/share.js
@@ -1,7 +1,7 @@
 import mail from './modules/Mail';
 import backToTop from './modules/BackToTop';
 import smoothScroll from './modules/SmoothScroll';
-import './modules/Observer';
+// import './modules/Observer';
 import './modules/Theme';
 
 mail();
